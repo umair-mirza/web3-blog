df9ce66524013559f8e12811d6814cb116948166
Umair Mirza <umair_hp@hotmail.com>
2022-07-10 18:42:08 +0500

    Removed the new keyword from edit/[id].js line: provider = ethers.providers.JsonRpcProvider(process.env.MUMBAI_TESTNET_RPC_URL)

--- pages/edit-post/[id].js
+++ pages/edit-post/[id].js
@@ -36,7 +36,7 @@ export default function Post() {
     if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'local') {
       provider = new ethers.providers.JsonRpcProvider()
     } else if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet') {
-      provider = new ethers.providers.JsonRpcProvider(process.env.MUMBAI_TESTNET_RPC_URL)
+      provider = ethers.providers.JsonRpcProvider(process.env.MUMBAI_TESTNET_RPC_URL)
     } else {
       provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/')
     }
